31f0c6a82b620aa5b3c929a1587e56feeab3cbf6
FasilCheema <faisal.cheema4@gmail.com>
2021-03-09 21:01:13 -0500

    added some folders and data

---
+++ Modules/Preprocesser.py
@@ -0,0 +1,8 @@
+'''
+Authors: Fasil Cheema and Mahmoud Alsaeed
+Purpose: This module preprocesses the data (tweets) in preperation to create a network
+'''
+
+class Preprocesser:
+
+    def initialize(self):
